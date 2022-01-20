ccda0a5229c159c6100171a2146adf969196ed00
8Firdavs8 <atabfi01@luther.edu>
2022-01-19 22:16:37 -0600

    Building components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+
+
+function Header (props) {
+  console.log(props)
+  return (
+    
+    <h1>{props.name} Book Club!</h1>
+  )
+}
+
+function Main () {
+  return (
+    <h2>We Read The Best Books!</h2>
+  )
+}
+
+function Footer() {
+  return (
+    <h3>Address and Copyright</h3>
+  )
+}
 
 function App() {
   return (
-    <div>Hello Wolrd!</div>
-  );
+    <div className = "App">
+     <Header name = "Jasmine's"/>
+     <Main />
+     <Footer />
+
+    </div>
+  )
+  
+  
 }
 
 export default App;
