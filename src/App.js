a54126c54eb2cb3c0a4e2ebf25f9e22d0be47a6d
8Firdavs8 <atabfi01@luther.edu>
2022-01-20 00:29:31 -0600

    more components

--- src/App.js
+++ src/App.js
@@ -6,29 +6,48 @@ import './App.css';
 function Header (props) {
   console.log(props)
   return (
+    <header>
+      <h1>{props.name} Kitchen</h1>
+    </header>
+  )
+}
+
+function Main (props) {
+  return (
+    <section>
+       <h2>We serve {props.adjective} food around. 
+        <ul style = {{textAlign: "left"}}>
+          {props.dishOjects.map((dishObjects, i) => 
+          (<li key = {i}>dishes</li>))}
+        </ul>
     
-    <h1>{props.name} Book Club!</h1>
+       </h2>
+    </section>
+   
   )
 }
+const dishObjects = dishes.map((dishes, i) => ({id: i , title: dishes}));
+console.log(dishObjects)
 
-function Main () {
+function Footer(props) {
   return (
-    <h2>We Read The Best Books!</h2>
+    <h3>Copyright {props.year}</h3>
   )
 }
+const dishes = [
+  "Macaroni and Cheese",
+  "Tofu",
+  "Salmon"
+]
 
-function Footer() {
-  return (
-    <h3>Address and Copyright</h3>
-  )
-}
+
 
 function App() {
   return (
     <div className = "App">
-     <Header name = "Jasmine's"/>
-     <Main />
-     <Footer />
+     <Header name = "Harico's"/>
+     <Main adjective = "best" dishes={dishes}/>
+     <Footer year = {new Date().getFullYear()}/>
 
     </div>
   )
